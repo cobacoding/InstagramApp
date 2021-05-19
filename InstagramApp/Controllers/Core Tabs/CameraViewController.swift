610fc8479c0398044c999afa4e92ed7f95aa866f
cobacoding <codingsuka@gmail.com>
2021-05-19 21:34:37 +0700

    add folder and file

---
+++ InstagramApp/Controllers/Core Tabs/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.swift
+//  InstagramApp
+//
+//  Created by Riky Kurniawan on 19/05/21.
+//  Copyright © 2021 Riky Developer App. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ InstagramApp/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by Riky Kurniawan on 19/05/21.
+//  Copyright © 2021 Riky Developer App. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
