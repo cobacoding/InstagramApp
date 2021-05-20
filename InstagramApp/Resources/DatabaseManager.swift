47827d891c637fc71653691c6bfc07d1b146f9d7
cobacoding <codingsuka@gmail.com>
2021-05-21 06:56:45 +0700

    DatabaseManager

---
+++ InstagramApp/Resources/DatabaseManager.swift
@@ -0,0 +1,22 @@
+//
+//  DatabaseManager.swift
+//  InstagramApp
+//
+//  Created by Riky Kurniawan on 21/05/21.
+//  Copyright © 2021 Riky Developer App. All rights reserved.
+//
+
+import FirebaseDatabase
+
+public class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    // MARK: - Public
+    public func registerNewUser(username: String, email: String, password: String) {
+        
+    }
+    
+    public func loginUser(username: String?, email: String?, password: String?) {
+        
+    }
+}
