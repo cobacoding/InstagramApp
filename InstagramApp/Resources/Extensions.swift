c2d3c00e73d1defba0ba767fa8d527e81ff76b17
cobacoding <codingsuka@gmail.com>
2021-06-03 07:29:46 +0700

    Add Extensions.swift and add background images header

---
+++ InstagramApp/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  InstagramApp
+//
+//  Created by Riky Kurniawan on 03/06/21.
+//  Copyright © 2021 Riky Developer App. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
